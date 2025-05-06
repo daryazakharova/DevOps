e11a00a0ea2b4abf09f870d70cd77c98314bc537
daryazakharova <95441230+daryazakharova@users.noreply.github.com>
2025-05-06 11:04:00 +0300

    Add files via upload

---
+++ DO5_SimpleDocker-1-develop/src/part6/container_part5/server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "fcgi_stdio.h"
+
+int main(void){
+while (FCGI_Accept() >= 0){
+printf("Content-type: text/html\r\n"
+           "\r\n"
+           "<title>Hello World!</title>"
+           "<h1>Hello World!</h1>");
+}
+return 0;
+}
